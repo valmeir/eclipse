05f340344578a4c2fa7915d457baf6d9bfcef3ac
valmeir <87622267+valmeir@users.noreply.github.com>
2021-09-19 20:21:56 -0300

    Adicionei arquivos via upload

---
+++ Hello/Hello GitHub/src/br/com/valeria/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.valeria.git;
+
+/**
+ * @author valme
+ *Exemplo de integração do eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
